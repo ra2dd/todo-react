e0186217d1400dd1baa868ee5c0ecc213707bbf5
ra2dd <98891234+ra2dd@users.noreply.github.com>
2023-04-20 21:19:18 +0200

    task adding functionality and task counting

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import React, { useState } from 'react';
+import { nanoid } from "nanoid";
 
 function App(props) 
 {
+    /*
+        Handing tasks
+    */
     const [tasks, setTasks] = useState(props.tasks);
 
     const taskList = tasks.map((task) => 
@@ -20,8 +24,13 @@ function App(props)
 
     function addTask(name)
     {
-        alert(name);
+        const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+        setTasks([...tasks, newTask]);
     }
+
+
+    const tasksNoun = tasks.length > 1 ? 'tasks' : 'task';
+    const headingText = `${tasks.length} ${tasksNoun} remaining`;
 
     return (
         <div className="todoapp stack-large">
@@ -35,7 +44,7 @@ function App(props)
                 <FilterButton />
             </div>
 
-            <h2 id="list-heading">... tasks remaining</h2>
+            <h2 id="list-heading">{ headingText }</h2>
             
             <ul
             role="list"
