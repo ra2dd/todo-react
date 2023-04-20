006be54807f36112889e5a72a6cf22f6e802fa50
ra2dd <98891234+ra2dd@users.noreply.github.com>
2023-04-20 20:55:06 +0200

    added state functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import React, { useState } from 'react';
 
 function App(props) 
 {
-    const taskList = props.tasks?.map((task) => 
+    const [tasks, setTasks] = useState(props.tasks);
+
+    const taskList = tasks.map((task) => 
         (
             <Todo 
                 id={ task.id } 
                 name={ task.name } 
                 completed={ task.completed } 
-                key= {task.id }
+                key= { task.id }
             />
         )
     );
+
+    function addTask(name)
+    {
+        alert(name);
+    }
 
     return (
         <div className="todoapp stack-large">
             <h1>Todo List</h1>
 
-            <Form />
-            <FilterButton />
+            <Form addTask={ addTask } />
+
+            <div className="filters btn-group stack-exception">
+                <FilterButton />
+                <FilterButton />
+                <FilterButton />
+            </div>
+
             <h2 id="list-heading">... tasks remaining</h2>
             
             <ul
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -3,25 +3,11 @@ import React from 'react';
 function FilterButton(props)
 {
     return(
-        <div className="filters btn-group stack-exception">
-            <button type="button" className="btn toogle-btn" aria-pressed="false">
-                <span className="visually-hidden">Show</span>
-                <span>all</span>
-                <span className="visually-hidden">tasks</span>
-            </button>
-
-            <button type="button" className="btn toogle-btn" aria-pressed="false">
-                <span className="visually-hidden">Show</span>
-                <span>Active</span>
-                <span className="visually-hidden">tasks</span>
-            </button>
-
-            <button type="button" className="btn toogle-btn" aria-pressed="false">
-                <span className="visually-hidden">Show</span>
-                <span>Completed</span>
-                <span className="visually-hidden">tasks</span>
-            </button>
-        </div>
+        <button type="button" className="btn toogle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show</span>
+            <span>{  }</span>
+            <span className="visually-hidden">tasks</span>
+        </button>
     )
 }
 
